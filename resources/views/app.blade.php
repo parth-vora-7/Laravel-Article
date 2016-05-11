8f56d079911561a55c21db12564132aa963372a8
xparthx <parth.vora.777@gmail.com>
2016-05-11 11:12:19 +0530

    removed extra CSRF hidden field

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token()}}">
-        {{ csrf_field()}}
         <title>Laravel</title>
         {!! Html::style('css/app.css') !!}
         {!! Html::style('bootstrap/css/bootstrap.min.css') !!}
